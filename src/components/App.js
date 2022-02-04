052214b72f7052f6ebf1766314f4df514cbc8b04
Lok Shang Ru <skatemonkey3124@gmail.com>
2022-02-04 16:37:30 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import Token from '../abis/Token.json'
+import EthSwap from '../abis/EthSwap.json'
+import Navbar from './Navbar'
+import Main from './Main'
+import './App.css'
+
+class App extends Component {
+
+  // this function will be executed first before the render function at below
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  // connect app to block chain through Metamask
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    // fetch the metamask account
+    const accounts = await web3.eth.getAccounts()
+
+    // console.log(accounts)
+    // store the account in the react state
+    // we first need to build a constructor.
+    this.setState({ account: accounts[0] })
+    // after storing the account in the react state, we can fetch the account like this
+    // Now we can fetch this account in anywhere we want
+    console.log(this.state.account)
+
+    // same concept as above
+    // store the account balance in the react state so we can call them in anywhere we want
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ ethBalance })
+    console.log(this.state.ethBalance)
+
+    // Load Token contract
+    const networkId = await web3.eth.net.getId()
+    const tokenData = Token.networks[networkId]
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address)
+      this.setState({ token })
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+      this.setState({ tokenBalance: tokenBalance.toString() })
+    } else {
+      window.alert('Token contract not deployed to detected network.')
+    }
+
+    // Load EthSwap contract
+    const ethSwapData = EthSwap.networks[networkId]
+    if (ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+      this.setState({ ethSwap })
+    } else {
+      window.alert('EthSwap contract not deployed to detected network.')
+    }
+
+    this.setState({ loading: false })
+  }
+
+
+
+  // function to buy token
+  buyTokens = (etherAmount) => {
+    this.setState({ loading: true })
+    this.state.ethSwap.methods.buyTokens().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  // function to sell token
+  // selling token is a 2 step process compare to buying token
+  sellTokens = (tokenAmount) => {
+    this.setState({ loading: true })
+    this.state.token.methods.approve(this.state.ethSwap.address, tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.state.ethSwap.methods.sellTokens(tokenAmount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+
+  // setting up react state
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      token: {},
+      ethSwap: {},
+      ethBalance: '0',
+      tokenBalance: '0',
+      loading: true
+    }
+  }
+
+  render() {
+    let content
+    if (this.state.loading) {
+      // if the app is loading, show the text "loading....."
+      content = <p id="loader" className="text-center">Loading...</p>
+    } else { // if the app is not loading, load the main component
+      content = <Main
+        ethBalance={this.state.ethBalance}
+        tokenBalance={this.state.tokenBalance}
+        buyTokens={this.buyTokens}
+        sellTokens={this.sellTokens}
+      />
+    }
+
+    return (
+      <div>
+        {/* load the navbar */}
+        {/* we need to pass down the account stored in react state */}
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                </a>
+
+                {content}
+
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
